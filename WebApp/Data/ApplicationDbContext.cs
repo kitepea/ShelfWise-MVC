c3b04bdfab868509b3f9e46234a61257f64bfa87
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-08-14 21:19:53 +0700

    Migrate database and create model

---
+++ WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
