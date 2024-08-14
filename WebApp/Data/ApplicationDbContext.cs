8772f034202265de4d4eabac7e21c751611a7d4b
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-08-14 22:14:01 +0700

    Add tab <li/>, seed data

--- WebApp/Data/ApplicationDbContext.cs
+++ WebApp/Data/ApplicationDbContext.cs
@@ -5,11 +5,18 @@ namespace WebApp.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Category 1", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Category 2", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Category 3", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Category 4", DisplayOrder = 4 }
+                );
+        }
     }
 }
