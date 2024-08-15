b3971481d4aa53609f72bc3e12822b5bf1ae7029
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-08-15 11:43:08 +0700

    feat: create category interface

--- WebApp/Controllers/CategoryController.cs
+++ WebApp/Controllers/CategoryController.cs
@@ -23,5 +23,11 @@ namespace WebApp.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            return View();
+        }
     }
 }
