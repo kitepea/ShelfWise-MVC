cfe02db678525acdf4fb6cebd5a6783e7ccfeb4d
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-08-17 23:25:25 +0700

    Add and implement IRepository
    * Index page width increased
    * Add IRepository interface, implement in Repository

---
+++ ShelfWise.DataAccess/Repository/Repository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShelfWise.DataAccess.Data;
+using ShelfWise.DataAccess.Repository.IRepository;
+using System.Linq.Expressions;
+
+namespace ShelfWise.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+            // _db.Categories == dbSet
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+    }
+}
