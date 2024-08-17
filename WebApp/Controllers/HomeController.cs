475041db127eefec7f567e937e16ff66cfb6b573
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-08-17 19:08:51 +0700

    Print out the messages

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text;
 using WebApp.Models;
 using WebApp.Services;
 
@@ -7,8 +8,6 @@ namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
         private readonly IScopedService _scoped1;
         private readonly IScopedService _scoped2;
 
@@ -18,14 +17,36 @@ namespace WebApp.Controllers
         private readonly ISingletonService _singleton1;
         private readonly ISingletonService _singleton2;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            IScopedService scoped1, IScopedService scoped2,
+            ITransientService transient1, ITransientService transient2,
+            ISingletonService singleton1, ISingletonService singleton2)
         {
-            _logger = logger;
+            _scoped1 = scoped1;
+            _scoped2 = scoped2;
+
+            _transient1 = transient1;
+            _transient2 = transient2;
+
+            _singleton1 = singleton1;
+            _singleton2 = singleton2;
         }
 
         public IActionResult Index()
         {
-            return View();
+            StringBuilder messages = new StringBuilder();
+            messages.AppendLine($"Transient 1: {_transient1.GetGuid()}\n");
+            messages.AppendLine($"Transient 2: {_transient2.GetGuid()}\n");
+            messages.AppendLine("Transient - A different instance of a resource, everytime it's requested (i.e. someone requests an ITransientService).\n\n");
+
+            messages.AppendLine($"Scoped 1: {_scoped1.GetGuid()}\n");
+            messages.AppendLine($"Scoped 2: {_scoped2.GetGuid()}\n");
+            messages.AppendLine("Scoped - One instance of a resource, but only for the current request. New request (i.e. hit an API endpoint again) = new instance\n\n");
+
+            messages.AppendLine($"Singleton 1: {_singleton1.GetGuid()}\n");
+            messages.AppendLine($"Singleton 2: {_singleton2.GetGuid()}\n");
+            messages.AppendLine("Singleton - One instance of a resource, for the lifetime of the application\n\n");
+            return Ok(messages.ToString());
         }
 
         public IActionResult Privacy()
