b094e7a512fb794f2a3b8d72182a3ea15453dbd2
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-08-17 23:49:02 +0700

    Add CategoryRepository, ICategoryRepository

---
+++ ShelfWise.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using ShelfWise.DataAccess.Data;
+using ShelfWise.DataAccess.Repository.IRepository;
+using ShelfWise.Models;
+
+namespace ShelfWise.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public ApplicationDbContext _db { get; }
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
---
+++ ShelfWise.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using ShelfWise.Models;
+
+namespace ShelfWise.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+        void Save();
+    }
+}
