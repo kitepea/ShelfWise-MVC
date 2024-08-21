0017250cd2bb33fa2aa16f4526e9fe8a856b82d7
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-08-21 16:46:33 +0700

    Add HomePage; Modify HomeController

--- WebApp/Areas/Customer/Controllers/HomeController.cs
+++ WebApp/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ShelfWise.DataAccess.Repository.IRepository;
 using ShelfWise.Models;
 using System.Diagnostics;
 
@@ -8,15 +9,18 @@ namespace WebApp.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(products);
         }
 
         public IActionResult Privacy()
