09b4b2f036f09d520435b504b41598e047de0ec5
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-08-23 00:54:22 +0700

    Implementing Authorization; Admin area restricted

--- WebApp/Areas/Admin/Controllers/ProductController.cs
+++ WebApp/Areas/Admin/Controllers/ProductController.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using ShelfWise.DataAccess.Repository.IRepository;
 using ShelfWise.Models;
 using ShelfWise.Models.ViewModels;
+using ShelfWise.Utils;
 
 namespace WebApp.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = StaticDetails.Role_Admin)]
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
