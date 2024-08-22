9d7d23be0fb6bf5c9a13a413de89e936e6c2f719
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-08-22 22:48:01 +0700

    Add ApplicationUser; Modify Register code-behind
    * Code behind create ApplicationUsers instead of IdentityUser

--- ShelfWise.DataAccess/Data/ApplicationDbContext.cs
+++ ShelfWise.DataAccess/Data/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace ShelfWise.DataAccess.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
