320557bf73957671f9e11bc2b4fcb9de7c804d0f
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-08-23 23:14:20 +0700

    Add Company Model

--- ShelfWise.DataAccess/Data/ApplicationDbContext.cs
+++ ShelfWise.DataAccess/Data/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ namespace ShelfWise.DataAccess.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Company> Companies { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
