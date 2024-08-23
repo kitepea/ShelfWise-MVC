5bad4bc372e45f8dbee9163bbcbb8ae168ff1700
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-08-23 23:19:14 +0700

    Add CompanyRepository
    * Modify UnitOfWork

---
+++ ShelfWise.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,21 @@
+﻿using ShelfWise.DataAccess.Data;
+using ShelfWise.DataAccess.Repository.IRepository;
+using ShelfWise.Models;
+
+namespace ShelfWise.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        public ApplicationDbContext _db { get; }
+
+        public CompanyRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Company obj)
+        {
+            _db.Companies.Update(obj);
+        }
+    }
+}
---
+++ ShelfWise.DataAccess/Repository/IRepository/ICompanyRepository.cs
@@ -0,0 +1,9 @@
+﻿using ShelfWise.Models;
+
+namespace ShelfWise.DataAccess.Repository.IRepository
+{
+    public interface ICompanyRepository : IRepository<Company>
+    {
+        void Update(Company obj);
+    }
+}
