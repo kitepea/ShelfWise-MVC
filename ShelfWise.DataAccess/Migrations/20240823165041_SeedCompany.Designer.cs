aa15b4b8e6f03674cfc0e3e9735cd3891a4c0246
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-08-23 23:53:55 +0700

    Seed Data for Company

--- ShelfWise.DataAccess/Migrations/20240823165041_SeedCompany.Designer.cs
+++ ShelfWise.DataAccess/Migrations/20240823165041_SeedCompany.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShelfWise.DataAccess.Data;
 
@@ -11,9 +12,11 @@ using ShelfWise.DataAccess.Data;
 namespace ShelfWise.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240823165041_SeedCompany")]
+    partial class SeedCompany
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
