5df0b75d996642349bb8a467c124466c699c5938
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-08-28 12:09:33 +0700

    Place order for Company accounts

--- WebApp/Areas/Customer/Controllers/CartController.cs
+++ WebApp/Areas/Customer/Controllers/CartController.cs
@@ -8,166 +8,178 @@ using System.Security.Claims;
 
 namespace WebApp.Areas.Customer.Controllers
 {
-	[Area("Customer")]
-	[Authorize]
-	public class CartController : Controller
-	{
-		private readonly IUnitOfWork _unitOfWork;
-		[BindProperty]
-		public ShoppingCartViewModel ShoppingCartViewModel { get; set; }
+    [Area("Customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        [BindProperty]
+        public ShoppingCartViewModel ShoppingCartViewModel { get; set; }
 
-		public CartController(IUnitOfWork unitOfWork)
-		{
-			_unitOfWork = unitOfWork;
-		}
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
 
-		public IActionResult Index()
-		{
-			var claimsIdentity = (ClaimsIdentity)User.Identity;
-			var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
-			ShoppingCartViewModel = new ShoppingCartViewModel()
-			{
-				ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product"),
-				OrderHeader = new()
-			};
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            ShoppingCartViewModel = new ShoppingCartViewModel()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product"),
+                OrderHeader = new()
+            };
 
-			foreach (var cart in ShoppingCartViewModel.ShoppingCartList)
-			{
-				cart.Price = GetPriceBasedOnQuantity(cart);
-				ShoppingCartViewModel.OrderHeader.OrderTotal += (cart.Price * cart.Count);
-			}
+            foreach (var cart in ShoppingCartViewModel.ShoppingCartList)
+            {
+                cart.Price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartViewModel.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+            }
 
-			return View(ShoppingCartViewModel);
-		}
+            return View(ShoppingCartViewModel);
+        }
 
-		public IActionResult Summary()
-		{
-			var claimsIdentity = (ClaimsIdentity)User.Identity;
-			var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
-			ShoppingCartViewModel = new ShoppingCartViewModel()
-			{
-				ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product"),
-				OrderHeader = new()
-			};
+            ShoppingCartViewModel = new ShoppingCartViewModel()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product"),
+                OrderHeader = new()
+            };
 
-			ShoppingCartViewModel.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userId);
-			ShoppingCartViewModel.OrderHeader.Name = ShoppingCartViewModel.OrderHeader.ApplicationUser.Name;
-			ShoppingCartViewModel.OrderHeader.PhoneNumber = ShoppingCartViewModel.OrderHeader.ApplicationUser.PhoneNumber;
-			ShoppingCartViewModel.OrderHeader.StreetAddress = ShoppingCartViewModel.OrderHeader.ApplicationUser.StreetAddress;
-			ShoppingCartViewModel.OrderHeader.City = ShoppingCartViewModel.OrderHeader.ApplicationUser.City;
-			ShoppingCartViewModel.OrderHeader.State = ShoppingCartViewModel.OrderHeader.ApplicationUser.State;
-			ShoppingCartViewModel.OrderHeader.PostalCode = ShoppingCartViewModel.OrderHeader.ApplicationUser.PostalCode;
+            ShoppingCartViewModel.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userId);
+            ShoppingCartViewModel.OrderHeader.Name = ShoppingCartViewModel.OrderHeader.ApplicationUser.Name;
+            ShoppingCartViewModel.OrderHeader.PhoneNumber = ShoppingCartViewModel.OrderHeader.ApplicationUser.PhoneNumber;
+            ShoppingCartViewModel.OrderHeader.StreetAddress = ShoppingCartViewModel.OrderHeader.ApplicationUser.StreetAddress;
+            ShoppingCartViewModel.OrderHeader.City = ShoppingCartViewModel.OrderHeader.ApplicationUser.City;
+            ShoppingCartViewModel.OrderHeader.State = ShoppingCartViewModel.OrderHeader.ApplicationUser.State;
+            ShoppingCartViewModel.OrderHeader.PostalCode = ShoppingCartViewModel.OrderHeader.ApplicationUser.PostalCode;
 
-			foreach (var cart in ShoppingCartViewModel.ShoppingCartList)
-			{
-				cart.Price = GetPriceBasedOnQuantity(cart);
-				ShoppingCartViewModel.OrderHeader.OrderTotal += (cart.Price * cart.Count);
-			}
+            foreach (var cart in ShoppingCartViewModel.ShoppingCartList)
+            {
+                cart.Price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartViewModel.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+            }
 
-			return View(ShoppingCartViewModel);
-		}
+            return View(ShoppingCartViewModel);
+        }
 
-		[HttpPost]
-		[ActionName("Summary")]
-		public IActionResult SummaryPOST(ShoppingCartViewModel shoppingCartViewModel)
-		{
-			var claimsIdentity = (ClaimsIdentity)User.Identity;
-			var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+        [HttpPost]
+        [ActionName("Summary")]
+        public IActionResult SummaryPOST(ShoppingCartViewModel ShoppingCartViewModel)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
-			ShoppingCartViewModel.ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product");
+            ShoppingCartViewModel.ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product");
 
-			ShoppingCartViewModel.OrderHeader.OrderDate = DateTime.Now;
-			ShoppingCartViewModel.OrderHeader.ApplicationUserId = userId;
+            ShoppingCartViewModel.OrderHeader.OrderDate = DateTime.Now;
+            ShoppingCartViewModel.OrderHeader.ApplicationUserId = userId;
 
-			ShoppingCartViewModel.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userId);
+            ApplicationUser applicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userId);
+            // Careful when populate navigation properties, ending up when saving the object, it will try to save the navigation property as a new entity as well
 
-			foreach (var cart in ShoppingCartViewModel.ShoppingCartList)
-			{
-				cart.Price = GetPriceBasedOnQuantity(cart);
-				ShoppingCartViewModel.OrderHeader.OrderTotal += (cart.Price * cart.Count);
-			}
+            foreach (var cart in ShoppingCartViewModel.ShoppingCartList)
+            {
+                cart.Price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartViewModel.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+            }
 
-			if (ShoppingCartViewModel.OrderHeader.ApplicationUser.CompanyId.GetValueOrDefault() == 0)
-			{
-				// Regular, not company
-				ShoppingCartViewModel.OrderHeader.OrderStatus = StaticDetails.StatusPending;
-				ShoppingCartViewModel.OrderHeader.PaymentStatus = StaticDetails.PaymentStatusPending;
-			}
-			else
-			{
-				// Company, Net 30
-				ShoppingCartViewModel.OrderHeader.OrderStatus = StaticDetails.StatusApproved;
-				ShoppingCartViewModel.OrderHeader.PaymentStatus = StaticDetails.PaymentStatusDelayedPayment;
-			}
+            if (applicationUser.CompanyId.GetValueOrDefault() == 0)
+            {
+                // Regular, not company
+                ShoppingCartViewModel.OrderHeader.OrderStatus = StaticDetails.StatusPending;
+                ShoppingCartViewModel.OrderHeader.PaymentStatus = StaticDetails.PaymentStatusPending;
+            }
+            else
+            {
+                // Company, Net 30
+                ShoppingCartViewModel.OrderHeader.OrderStatus = StaticDetails.StatusApproved;
+                ShoppingCartViewModel.OrderHeader.PaymentStatus = StaticDetails.PaymentStatusDelayedPayment;
+            }
 
-			_unitOfWork.OrderHeader.Add(ShoppingCartViewModel.OrderHeader);
-			_unitOfWork.Save();
+            _unitOfWork.OrderHeader.Add(ShoppingCartViewModel.OrderHeader);
+            _unitOfWork.Save();
 
-			foreach (var cart in ShoppingCartViewModel.ShoppingCartList)
-			{
-				OrderDetail orderDetail = new()
-				{
-					OrderHeaderId = ShoppingCartViewModel.OrderHeader.Id,
-					ProductId = cart.ProductId,
-					Price = cart.Price,
-					Count = cart.Count
-				};
-				_unitOfWork.OrderDetails.Add(orderDetail);
-				_unitOfWork.Save();
-			}
+            foreach (var cart in ShoppingCartViewModel.ShoppingCartList)
+            {
+                OrderDetail orderDetail = new()
+                {
+                    OrderHeaderId = ShoppingCartViewModel.OrderHeader.Id,
+                    ProductId = cart.ProductId,
+                    Price = cart.Price,
+                    Count = cart.Count
+                };
+                _unitOfWork.OrderDetails.Add(orderDetail);
+                _unitOfWork.Save();
+            }
 
-			return View(ShoppingCartViewModel);
-		}
+            if (applicationUser.CompanyId.GetValueOrDefault() == 0)
+            {
+                // Regular, not company; Adding stripe logic
 
-		public IActionResult Plus(int cartId)
-		{
-			var cartFromDb = _unitOfWork.ShoppingCart.Get(c => c.Id == cartId);
-			cartFromDb.Count += 1;
-			_unitOfWork.ShoppingCart.Update(cartFromDb);
-			_unitOfWork.Save();
-			return RedirectToAction(nameof(Index));
-		}
+            }
 
-		public IActionResult Minus(int cartId)
-		{
-			var cartFromDb = _unitOfWork.ShoppingCart.Get(c => c.Id == cartId);
-			if (cartFromDb.Count <= 1)
-			{
-				_unitOfWork.ShoppingCart.Remove(cartFromDb);
-			}
-			else
-			{
-				cartFromDb.Count -= 1;
-				_unitOfWork.ShoppingCart.Update(cartFromDb);
-			}
+            return RedirectToAction(nameof(OrderConfirmation), new { orderId = ShoppingCartViewModel.OrderHeader.Id });
+        }
 
-			_unitOfWork.Save();
-			return RedirectToAction(nameof(Index));
-		}
+        public IActionResult OrderConfirmation(int orderId)
+        {
+            return View(orderId);
+        }
 
-		public IActionResult Remove(int cartId)
-		{
-			var cartFromDb = _unitOfWork.ShoppingCart.Get(c => c.Id == cartId);
-			_unitOfWork.ShoppingCart.Remove(cartFromDb);
-			_unitOfWork.Save();
-			return RedirectToAction(nameof(Index));
-		}
+        public IActionResult Plus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(c => c.Id == cartId);
+            cartFromDb.Count += 1;
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
 
-		private double GetPriceBasedOnQuantity(ShoppingCart cart)
-		{
-			if (cart.Count <= 50)
-			{
-				return cart.Product.Price;
-			}
-			else if (cart.Count <= 100)
-			{
-				return cart.Product.Price50;
-			}
-			else
-			{
-				return cart.Product.Price100;
-			}
-		}
-	}
+        public IActionResult Minus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(c => c.Id == cartId);
+            if (cartFromDb.Count <= 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            }
+            else
+            {
+                cartFromDb.Count -= 1;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(c => c.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private double GetPriceBasedOnQuantity(ShoppingCart cart)
+        {
+            if (cart.Count <= 50)
+            {
+                return cart.Product.Price;
+            }
+            else if (cart.Count <= 100)
+            {
+                return cart.Product.Price50;
+            }
+            else
+            {
+                return cart.Product.Price100;
+            }
+        }
+    }
 }
