2d844e0bbd55725e487cae0720966bb390a880a3
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-08-29 15:25:59 +0700

    Add SessionId; Add helper method in OrderHeaderRepo

--- ShelfWise.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
+++ ShelfWise.DataAccess/Repository/IRepository/IOrderHeaderRepository.cs
@@ -2,8 +2,10 @@
 
 namespace ShelfWise.DataAccess.Repository.IRepository
 {
-	public interface IOrderHeaderRepository : IRepository<OrderHeader>
-	{
-		void Update(OrderHeader obj);
-	}
+    public interface IOrderHeaderRepository : IRepository<OrderHeader>
+    {
+        void Update(OrderHeader obj);
+        void UpdateStatus(int id, string orderStatus, string? paymentStatus = null);
+        void UpdateStripePaymentId(int id, string sessionId, string paymentIntentId);
+    }
 }
--- ShelfWise.DataAccess/Repository/OrderHeaderRepository.cs
+++ ShelfWise.DataAccess/Repository/OrderHeaderRepository.cs
@@ -4,18 +4,44 @@ using ShelfWise.Models;
 
 namespace ShelfWise.DataAccess.Repository
 {
-	public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
-	{
-		public ApplicationDbContext _db { get; }
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        public ApplicationDbContext _db { get; }
 
-		public OrderHeaderRepository(ApplicationDbContext db) : base(db)
-		{
-			_db = db;
-		}
+        public OrderHeaderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
 
-		public void Update(OrderHeader obj)
-		{
-			_db.OrderHeaders.Update(obj);
-		}
-	}
+        public void Update(OrderHeader obj)
+        {
+            _db.OrderHeaders.Update(obj);
+        }
+
+        public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+        {
+            var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            if (orderFromDb != null)
+            {
+                orderFromDb.OrderStatus = orderStatus;
+                if (!String.IsNullOrEmpty(paymentStatus))
+                {
+                    orderFromDb.PaymentStatus = paymentStatus;
+                }
+            }
+        }
+
+        public void UpdateStripePaymentId(int id, string sessionId, string paymentIntentId)
+        {
+            var orderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            if (!String.IsNullOrEmpty(sessionId))
+            {
+                orderFromDb.SessionId = sessionId;
+            }
+            if (!String.IsNullOrEmpty(paymentIntentId))
+            {
+                orderFromDb.PaymentIntentId = paymentIntentId;
+            }
+        }
+    }
 }
