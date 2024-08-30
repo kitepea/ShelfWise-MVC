9b739ea1c06f2a2656de59df8e227e7315cec57a
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-08-30 11:04:07 +0700

    Add Action for Order Filtering

--- WebApp/Areas/Admin/Controllers/OrderController.cs
+++ WebApp/Areas/Admin/Controllers/OrderController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShelfWise.DataAccess.Repository.IRepository;
 using ShelfWise.Models;
+using ShelfWise.Utils;
 
 namespace WebApp.Areas.Admin.Controllers
 {
@@ -22,9 +23,27 @@ namespace WebApp.Areas.Admin.Controllers
         #region API CALLS
 
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
-            List<OrderHeader> orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            IEnumerable<OrderHeader> orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaders = orderHeaders.Where(o => o.PaymentStatus == StaticDetails.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    orderHeaders = orderHeaders.Where(o => o.OrderStatus == StaticDetails.StatusInProcess);
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(o => o.OrderStatus == StaticDetails.StatusApproved);
+                    break;
+                case "completed":
+                    orderHeaders = orderHeaders.Where(o => o.OrderStatus == StaticDetails.StatusShipped);
+                    break;
+                default:
+                    break;
+            }
             return Json(new { data = orderHeaders });
         }
 
--- WebApp/wwwroot/js/order.js
+++ WebApp/wwwroot/js/order.js
@@ -1,16 +1,31 @@
 ﻿var dataTable;
 $(function () {
-    loadDataTable();
+    var url = window.location.search;
+    if (url.includes("inprocess")) {
+        loadDataTable("inprocess");
+    }
+    else if (url.includes("pending")) {
+        loadDataTable("pending");
+    }
+    else if (url.includes("completed")) {
+        loadDataTable("completed");
+    }
+    else if (url.includes("approved")) {
+        loadDataTable("approved");
+    }
+    else {
+        loadDataTable("all");
+    }
 });
 
-function loadDataTable() {
+function loadDataTable(status) {
     dataTable = $('#orderTable').DataTable({
-        "ajax": { url: '/admin/order/getall' },
+        "ajax": { url: '/admin/order/getall?status=' + status },
         "columns": [
             { data: "id", "width": "5%" },
-            { data: "name", "width": "15%" },
-            { data: "phoneNumber", "width": "20%" },
-            { data: "applicationUser.email", "width": "15%" },
+            { data: "name", "width": "25%" },
+            { data: "phoneNumber", "width": "16%" },
+            { data: "applicationUser.email", "width": "24%" },
             { data: "orderStatus", "width": "10%" },
             { data: "orderTotal", "width": "10%" },
             {
@@ -20,7 +35,7 @@ function loadDataTable() {
                                 <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2 w-25 px-0"> <i class="bi bi-pencil-square"></i></a>
                             </div>`
                 },
-                "width": "25%"
+                "width": "10%"
             }
         ]
     });
