e511f7f4dd4aba14d32667bca86e4fec374e80b6
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-09-07 10:20:53 +0700

    Add FB AccessDenied

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -28,6 +28,7 @@ builder.Services.AddAuthentication().AddFacebook(facebookOptions =>
 {
     facebookOptions.AppId = builder.Configuration.GetSection("Facebook:AppId").Get<string>();
     facebookOptions.AppSecret = builder.Configuration.GetSection("Facebook:AppSecret").Get<string>();
+    facebookOptions.AccessDeniedPath = $"/Identity/Account/AccessDenied";
 });
 
 builder.Services.AddDistributedMemoryCache();
