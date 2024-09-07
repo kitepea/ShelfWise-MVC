6fef2deab96cb0c25c5b50b807ecc8cebc6eeb3f
Đậu Đức Quân <bruceparkerblue@gmail.com>
2024-09-07 16:26:44 +0700

    Create User Manage Controller and View

---
+++ WebApp/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShelfWise.DataAccess.Data;
+using ShelfWise.Models;
+using ShelfWise.Utils;
+
+namespace WebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = StaticDetails.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> allUserList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+            var userRoles = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            foreach (var user in allUserList)
+            {
+                var roleId = userRoles.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                var role = roles.FirstOrDefault(u => u.Id == roleId);
+            }
+            return Json(new { data = allUserList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+
+            return Json(new { success = true, message = "Delete successfully" });
+        }
+        #endregion
+
+    }
+}
---
+++ WebApp/wwwroot/js/user.js
@@ -0,0 +1,26 @@
+﻿var dataTable;
+$(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#userTable').DataTable({
+        "ajax": { url: '/admin/user/getall' },
+        "columns": [
+            { data: "name", "width": "25%" },
+            { data: "email", "width": "15%" },
+            { data: "phoneNumber", "width": "10%" },
+            { data: "company.name", "width": "15%" },
+            { data: "email", "width": "10%" },
+            {
+                data: "id",
+                "render": function (data) {
+                    return `<div class="w-100 btn-group d-flex justify-content-center" role="group">
+                                <a href="/admin/user/upsert?id=${data}" class="btn btn-primary mx-2 w-25 px-0"> <i class="bi bi-pencil-square"></i> Edit</a>
+                            </div>`
+                },
+                "width": "25%"
+            }
+        ]
+    });
+}
